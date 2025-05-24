069373ba075c4957cca0c8a06125e61c1e2a96d8
Rinoyt <Sasha1Sim5@gmail.com>
2025-05-24 13:16:24 +0300

    moved webSocket config file to config package

--- backend/src/main/java/ru/willBeEdited/scrabble/config/WebSocketConfig.java
+++ backend/src/main/java/ru/willBeEdited/scrabble/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package ru.willBeEdited.scrabble.webSocket;
+package ru.willBeEdited.scrabble.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
