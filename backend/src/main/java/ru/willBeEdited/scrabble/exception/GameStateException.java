00547554fcafc56ee0de560d5bb92f1b6f04e123
Rinoyt <Sasha1Sim5@gmail.com>
2025-05-24 16:02:57 +0300

    GameStateException added

---
+++ backend/src/main/java/ru/willBeEdited/scrabble/exception/GameStateException.java
@@ -0,0 +1,7 @@
+package ru.willBeEdited.scrabble.exception;
+
+public class GameStateException extends RuntimeException {
+    public GameStateException(String message) {
+        super(message);
+    }
+}
